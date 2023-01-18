bca659d82658d58e6cc286a0f522aba5a681e0dd
Pavan Ingalagi <ingalagipavan@gmail.com>
2023-01-19 00:17:56 +0530

    day-3

--- backend/routes/usersRoute.js
+++ backend/routes/usersRoute.js
@@ -5,6 +5,7 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const {authenticate} = require('../middlewares/authenticate.middleware');
+const { query } = require('express');
 
 usersRoute.get("/",(req,res)=>{
 res.send("you are in users route");
@@ -59,15 +60,59 @@ usersRoute.post("/login",async(req,res)=>{
         res.send({"msg": "Invalid Credentials"});
     }
 });
-usersRoute.use(authenticate);
-usersRoute.patch("/update/:id", async(req,res)=>{
-    try {
-        const id = req.params.id;
-        
 
+usersRoute.use(authenticate);
+
+usersRoute.patch("/update", async(req,res)=>{
+    try {
+        const {userID,f_name,l_name,email,pass,mobile} = req.body;
+        const new_data = req.body;
+        const user = await usersModel.findById({_id:userID});
+        if(user){
+            // console.log(new_data);
+            // res.send(new_data); 
+            if(pass){
+                const saltRounds = 10;
+                const myPlaintextPassword = user.pass;
+              bcrypt.hash(myPlaintextPassword, saltRounds, async(err, secured_pass)=> {
+                // Store hash in your password DB.
+                if(secured_pass){
+                    const data = await usersModel.findByIdAndUpdate(userID);
+                        data.pass = secured_pass;
+                        await data.save();
+                    res.send({"msg": "User data updated successfully"});
+                }else{
+                    res.send(err.message);
+                    res.send({"msg": "error updating user data"});
+                }
+            });
+
+            }else if(new_data){
+                const data = await usersModel.findByIdAndUpdate(userID, new_data);
+                        await data.save();
+                    res.send({"msg": "User data updated successfully"});
+                }else{
+                    res.send(err);
+                    res.send({"msg": "Invalid entry"});
+                }
+        }
+        
+    } catch (error) {
+        res.send({"msg": "Invalid entry"});
+        res.send(error);
+    }
+
+});
+
+usersRoute.delete("/delete", async(req,res)=>{
+    try {
+        const {userID} = req.body;
+        const data = await usersModel.findByIdAndDelete({"_id":userID});
+       res.send({"msg":"User deleted successfully"})
         
     } catch (error) {
         res.send(error);
+        res.send({"msg":"You are not authorized"})
     }
 
 });
