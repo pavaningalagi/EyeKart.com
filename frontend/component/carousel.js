7ed6475d661cbfa9456e21c688d186675e67cc29
Pavan Ingalagi <ingalagipavan@gmail.com>
2023-01-20 02:19:43 +0530

    frontend loading page

--- frontend/component/carousel.js
+++ frontend/component/carousel.js
@@ -35,9 +35,9 @@ const carousel = ()=>{
 
 carousel();
 
-
 const trendone = ()=>{
     let trend_div= document.getElementById("trend_rhs_div");
+        trend_div.innerHTML = null;
     let images= ['https://static1.lenskart.com/media/desktop/img/Sep21/image179.png',
                 'https://static1.lenskart.com/media/desktop/img/Sep21/cateeye.jpg',
                 'https://static1.lenskart.com/media/desktop/img/Sep21/clubmaster.jpg',
@@ -52,51 +52,137 @@ const trendone = ()=>{
     let imgElement0 = document.createElement('img');
         imgElement0.src =images[0];
     let p0 = document.createElement('p');
-        p0.innerText="Blend Edit";
-    let btn1 = document.createElement('button');
-        btn1.innerText="Explore";
-        btn1.onclick=()=>{
-            window.location.href="https://www.lenskart.com/eyeglasses/brands/vincent-chase-eyeglasses/blend-edit.html"
+        p0.innerText="Round";
+    let btn0 = document.createElement('button');
+        btn0.innerText="Explore";
+        btn0.onclick=()=>{
+            window.location.href="https://www.lenskart.com/eyeglasses/frame-shape/round.html"
         }
-        div_element0.append(imgElement0,p0,btn1);
+        div_element0.append(imgElement0,p0,btn0);
 
     let div_element1 = document.createElement('div');
         div_element1.setAttribute('class', "trendelement");
     let imgElement1 = document.createElement('img');
         imgElement1.src =images[1];
     let p1 = document.createElement('p');
-        p1.innerText="Air Clip On";
-        div_element1.append(imgElement1,p1);
+        p1.innerText="Cat-Eye";
+    let btn1 = document.createElement('button');
+        btn1.innerText="Explore";
+        btn1.onclick=()=>{
+            window.location.href="https://www.lenskart.com/eyeglasses/brands/vincent-chase-eyeglasses/cat-eye.html"
+        }
+        div_element1.append(imgElement1,p1,btn1);
 
     let div_element2 = document.createElement('div');
         div_element2.setAttribute('class', "trendelement");
     let imgElement2 = document.createElement('img');
         imgElement2.src =images[2];
-        div_element2.append(imgElement2);
+    let p2 = document.createElement('p');
+        p2.innerText="Clubmaster";
+    let btn2 = document.createElement('button');
+        btn2.innerText="Explore";
+        btn2.onclick=()=>{
+            window.location.href="https://www.lenskart.com/eyewear/promotion/frame-shape/clubmasters.html"
+        }
+        div_element2.append(imgElement2,p2,btn2);
         
     let div_element3 = document.createElement('div');
         div_element3.setAttribute('class', "trendelement");
     let imgElement3 = document.createElement('img');
         imgElement3.src =images[3];
-        div_element3.append(imgElement3);
+    let p3 = document.createElement('p');
+        p3.innerText="Transparent";
+    let btn3 = document.createElement('button');
+        btn3.innerText="Explore";
+        btn3.onclick=()=>{
+            window.location.href="https://www.lenskart.com/eyeglasses/promotions/vc-transparent.html"
+        }
+        div_element3.append(imgElement3,p3,btn3);
+
+        
+        trend_div.append(div_element0,div_element1,div_element2,div_element3);
+};
+
+const trendtwo = ()=>{
+    let trend_div= document.getElementById("trend_rhs_div");
+        trend_div.innerHTML = null;
+    let images= ['https://static1.lenskart.com/media/desktop/img/Sep21/image179.png',
+                'https://static1.lenskart.com/media/desktop/img/Sep21/cateeye.jpg',
+                'https://static1.lenskart.com/media/desktop/img/Sep21/clubmaster.jpg',
+                'https://static1.lenskart.com/media/desktop/img/Sep21/trans.jpg',
+                'https://static1.lenskart.com/media/desktop/img/Sep21/blend.jpg',
+                'https://static1.lenskart.com/media/desktop/img/Sep21/clipon.jpg',
+                'https://static1.lenskart.com/media/desktop/img/Sep21/airflex.jpg',
+                'https://static1.lenskart.com/media/desktop/img/Sep21/aviator.jpg'
+                ];
+    let div_element0 = document.createElement('div');
+        div_element0.setAttribute('class', "trendelement");
+    let imgElement0 = document.createElement('img');
+        imgElement0.src =images[4];
+    let p0 = document.createElement('p');
+        p0.innerText="Blend Edit";
+    let btn0 = document.createElement('button');
+        btn0.innerText="Explore";
+        btn0.onclick=()=>{
+            window.location.href="https://www.lenskart.com/eyeglasses/brands/vincent-chase-eyeglasses/blend-edit.html"
+        }
+        div_element0.append(imgElement0,p0,btn0);
+
+    let div_element1 = document.createElement('div');
+        div_element1.setAttribute('class', "trendelement");
+    let imgElement1 = document.createElement('img');
+        imgElement1.src =images[5];
+    let p1 = document.createElement('p');
+        p1.innerText="Air Clip On";
+    let btn1 = document.createElement('button');
+        btn1.innerText="Explore";
+        btn1.onclick=()=>{
+            window.location.href="https://www.lenskart.com/eyeglasses/brands/vincent-chase-eyeglasses/air-clip-on.html"
+        }
+        div_element1.append(imgElement1,p1,btn1);
+
+    let div_element2 = document.createElement('div');
+        div_element2.setAttribute('class', "trendelement");
+    let imgElement2 = document.createElement('img');
+        imgElement2.src =images[6];
+    let p2 = document.createElement('p');
+        p2.innerText="Air Flex";
+    let btn2 = document.createElement('button');
+        btn2.innerText="Explore";
+        btn2.onclick=()=>{
+            window.location.href="https://www.lenskart.com/eyeglasses/brands/vincent-chase-eyeglasses/air-flex.html"
+        }
+        div_element2.append(imgElement2,p2,btn2);
+        
+    let div_element3 = document.createElement('div');
+        div_element3.setAttribute('class', "trendelement");
+    let imgElement3 = document.createElement('img');
+        imgElement3.src =images[7];
+    let p3 = document.createElement('p');
+        p3.innerText="Retro Aviator";
+    let btn3 = document.createElement('button');
+        btn3.innerText="Explore";
+        btn3.onclick=()=>{
+            window.location.href="https://www.lenskart.com/eyeglasses/brands/vincent-chase-eyeglasses/aviator.html"
+        }
+        div_element3.append(imgElement3,p3,btn3);
 
         
         trend_div.append(div_element0,div_element1,div_element2,div_element3);
 };
 
 trendone();
-export {carousel, trendone};
+const leftarrow = document.getElementById('leftarrow');
+      leftarrow.onclick = () => {
+        trendone();
+    };
+
+const rightarrow = document.getElementById('rightarrow');
+    rightarrow.onclick = () => {
+        trendtwo();
+    };
 
 
-let imgElement4 = document.createElement('img');
-imgElement4.setAttribute('class', "trendelement");
-imgElement4.src =images[4];
-let imgElement5 = document.createElement('img');
-imgElement5.setAttribute('class', "trendelement");
-imgElement5.src =images[5];
-let imgElement6 = document.createElement('img');
-imgElement6.setAttribute('class', "trendelement");
-imgElement6.src =images[6];
-let imgElement7 = document.createElement('img');
-imgElement7.setAttribute('class', "trendelement");
-imgElement7.src =images[7];
+export {carousel, trendone, trendtwo};
+
+
--- frontend/component/navbar.js
+++ frontend/component/navbar.js
@@ -1,5 +1,53 @@
 const navbar = ()=>{
-    const nav = document.getElementById('navbar');
-    
-
+    `<div id="country">
+    <div id="country_lhs">
+        <p>Do More, Be More  |</p>
+        <p>Try in 3D  |</p>
+        <p>Store Locator  |</p>
+        <p>Quality  |</p>
+        <p>USA  |</p>
+        <p>Singapore  |</p>
+        <p>UAE  |</p>
+        <p>John Jacobs  |</p>
+        <p>Aqualens  |</p>
+        <p>Cobrowsing  |</p>
+        <p>Engineering Blog  |</p>
+        <p>Lenskart Franchise</p>
+    </div>
+    <div id="country_rhs">
+        <p>Contact Us</p>
+    </div>
+</div>
+<div id="navbar">
+    <div id="logodiv">
+        <img id="logo" src="./images/EyeKart_logo.png" alt="">
+    </div>
+    <div id="mid_nav">
+        <p id="phone"><i class="fa-solid fa-phone-volume"></i> 1800-111-111</p>
+        <input id="nav_input" type="text" placeholder="What are you looking for">
+    </div>
+    <div id="rhs_nav">
+        <button>Track Order</button>
+        <button>Sign In & Sign Up</button>
+        <button><i class="fa-regular fa-heart"></i></i> Wishlist</button>
+        <button><i class="fa-solid fa-cart-shopping"></i> Cart</button>
+    </div>
+</div>
+<div id="nav_opt">
+    <div id="nav_opt_lhs">
+        <p>EYEGLASSES</p>
+        <p>COMPUTER GLASES</p>
+        <p>KIDS GLASSES</p>
+        <p>CONTACT LENSES</p>
+        <p>SUNGLASSES</p>
+        <p>HOME EYE-TEST</p>
+        <p>STORE LOCATOR</p>
+    </div>
+    <div id="nav_opt_rhs">
+        <img src="https://static1.lenskart.com/media/desktop/img/May22/3dtryon1.png" alt="">
+        <img src="https://static1.lenskart.com/media/desktop/img/Mar22/13-Mar/blulogo.png" alt="">
+        <img src="https://static.lenskart.com/media/desktop/img/Feb22/18-Feb/goldlogo.jpg" alt="">
+    </div>
+</div> `
 };
+export {navbar};
