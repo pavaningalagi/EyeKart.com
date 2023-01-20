3b46f053be91740f4b29ee40d9ca1d214526a70f
Pavan Ingalagi <ingalagipavan@gmail.com>
2023-01-21 02:04:11 +0530

    day-4

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,12 +4,36 @@ import {carousel,trendone} from '../component/carousel.js';
 import { footer } from '../component/footer.js';
 
 const nav = document.getElementById('nava');
-      // nav.innerHTML = navbar();
+      nav.innerHTML = navbar();
 const ft = document.getElementById('ft');
       ft.innerHTML =footer();
 
+const loginbtn = document.getElementById('loginbtn');
+      loginbtn.onclick=() =>{
+            document.querySelector(".popup").classList.add('active');
+      };
+document.querySelector('.popup .close-btn').addEventListener("click", () =>{
+      document.querySelector(".popup").classList.remove('active');
+});
 
 
+const create_acc = document.getElementById('create_acc');
+      create_acc.onclick = () =>{
+            document.querySelector(".popup").classList.remove('active');
+            document.querySelector(".popups").classList.add('active');
+      };
+document.querySelector('.popups .close-btns').addEventListener("click", () =>{
+            document.querySelector(".popups").classList.remove('active');
+      });
+
+const signin = document.getElementById('signin');
+      signin.onclick=() =>{
+            document.querySelector(".popups").classList.remove('active');
+            document.querySelector(".popup").classList.add('active');
+      };
+document.querySelector('.popup .close-btn').addEventListener("click", () =>{
+document.querySelector(".popup").classList.remove('active');
+});
 
 
       
